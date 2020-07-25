d77d53e726a000a200d3e3becf53538b98fa38bc
harfang <raupasto@gmail.com>
2020-07-25 18:57:36 +0200

    memory mis à jour, deck ok

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,8 +1,12 @@
 var overlay = document.getElementById('overlay');
 var rules = document.getElementById('content-center');
 var buttonClose = document.getElementById('buttonClose');
-var turnBack = document.getElementById('return')
-var hard = document.getElementById('hard')
+var closeButton = document.getElementById('return');
+var deckLevel = document.getElementById('deckLevel');
+var hp = document.getElementById('hp');
+var sw = document.getElementById('sw');
+var sh = document.getElementById('sh');
+
 
 buttonClose.addEventListener('click', close);
 
@@ -19,18 +23,36 @@ function close(){
 function choiceLevel(choice){
 
    if (choice.textContent == "Facile"){
-  facile.style.display = 'block';
-  overlay.style.display ='none'; 
+    deckLevel.style.display = 'block';
+    overlay.style.display ='none'; 
+    hp.style.display ='block';
+    sw.style.display ='none';
+    sh.style.display ='none';   
+
   }
   else if (choice.textContent == "Moyen"){
-    medium.style.display = 'block';
+    deckLevel.style.display = 'block';
     overlay.style.display ='none'; 
+    hp.style.display ='none';
+    sw.style.display ='block';
+    sh.style.display ='none'; 
   }
   else if (choice.textContent == "Difficile"){ 
-  hard.style.display = 'block';
-  overlay.style.display ='none'; 
+    deckLevel.style.display = 'block';
+    overlay.style.display ='none'; 
+    hp.style.display ='none';
+    sw.style.display ='none';
+    sh.style.display ='block'; 
   
  }
+}
+
+closeButton.addEventListener('click', turnBack);
+
+function turnBack(){
+  deckLevel.style.display = 'none';
+  overlay.style.display = 'block';
+
 }
 
 $(".hover").mouseleave(
@@ -38,3 +60,76 @@ $(".hover").mouseleave(
       $(this).removeClass("hover");
     }
   );
+
+var deck = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+    .map ( p =>[p,Math.random()])
+    .sort ((deck,b) => deck[1]-b[1])
+    .map(p => p[0])
+
+console.log(deck);
+
+var img =document.getElementsByTagName('img');
+var endScore = document.getElementById('score');
+var score =0;
+var step =1;
+var p1,p2;
+var timer = null;
+
+
+for(let i=0; i <img.length; i++){
+    img[i].src2 = '../../picture/harry_potter/wizard' + deck[i] + '.jpg';
+    
+    }
+
+document.addEventListener('click', function(e){
+
+    switch(step){
+        case 1: /* premier clique*/ 
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p1 = e.target;
+                step = 2;
+            }
+
+            break;
+
+        case 2: /*deuxième clique*/ 
+            if (e.target.tagName=='IMG'){
+                e.target.src = e.target.src2;
+                p2 = e.target;
+                step = 3;
+            }
+            timer = setTimeout(check, 800);
+            break; 
+
+        case 3:
+            
+            clearTimeout(timer);
+            check();
+
+            break;
+    }
+   
+}); 
+
+function check(){
+    if (p1.src2==p2.src2){
+        p1.replaceWith( document.createElement('span'))
+        p2.replaceWith( document.createElement('span'))
+        score +=10;
+    }
+
+    else{
+        p2.src = p1.src ='./../picture/backside/wizzard.jpg';
+        score = Math.max(0,score-2);
+    }
+    
+    step =1;
+    endScore.textContent =score;
+    if (document.getElementsByTagName('img').length==0){
+        endScore.textContent += "BIEN JOUÉ BG ! ";
+
+    }
+
+}
+        
